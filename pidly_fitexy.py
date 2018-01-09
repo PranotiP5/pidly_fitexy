6843cdf8de6ce474565ccd1ff9cdb24079eea40e
PranotiP5 <35248653+PranotiP5@users.noreply.github.com>
2018-01-09 04:55:21 +0000

    Update pidly_fitexy.py

--- pidly_fitexy.py
+++ pidly_fitexy.py
@@ -4,20 +4,22 @@ import pidly
 idl=pidly.IDL('/opt/idl/exelis/idl85/bin/idl')#give correct path to executable idl file
 
 #create python variables or arrays
-x1=[1,2,3]
-ex1=[0.1,0.1,0.1]
+xp=[1,2,3]
+yp=[1,2,3]
+exp=[0.1,0.1,0.1]
+eyp=[0.1,0.1,0.1]
 
 #create idl arrays
 idl('x=[]')
-idl('y=x')
-idl('ex=x')
-idl('ey=x')
+idl('y=[]')
+idl('ex=[]')
+idl('ey=[]')
 
 #assign python variables to IDL variables
-idl.x=x1
-idl.y=idl.x
-idl.ex=ex1
-idl.ey=idl.x
+idl.x=xp
+idl.y=yp
+idl.ex=exp
+idl.ey=eyp
 
 #use procedure with IDl variables
 idl('FITEXY,x,y,A,B,X_SIG=ex,Y_SIG=ey, sigma_A_B, chi_sq, q')
@@ -27,7 +29,6 @@ print idl.sigma_A_B[0]
 
 #end session
 idl.close()
-
 
 
 
